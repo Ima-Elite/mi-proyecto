feeac4e7aab80ef2c1af09c91a96bf74749cc188
Fernando <f.sierra@viavox.com>
2023-06-06 11:49:30 +0200

    primer commit

--- config/app.php
+++ config/app.php
@@ -7,7 +7,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Application Name Hola prueba
+    | Application Name
     |--------------------------------------------------------------------------
     |
     | This value is the name of your application. This value is used when the
